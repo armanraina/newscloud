100915db7e33e7700263954c9868da8e1e0f3dd4
root <arman.raina@yahoo.com>
2017-08-11 11:11:29 -0700

    add bootsrap

--- scripts/main.js
+++ scripts/main.js
@@ -12,11 +12,11 @@ for (var i=list.length-1;i>=0;i--){
 function filterList(list,paper){
 	var forbid = {
 	"jbn":["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec","Fall","Winter","Spring","Summer","Subscribe","back"],
-	"nyt":["AM","ET","Comments","Comment","New","PM","time","site"],
+	"nyt":["AM","ET","Comments","Comment","New","PM","time","site","comment","Site"],
 	"dmk":[],
 	"gdn":["world","after","Edit","amp","home"],
-	"bbt":["comments","sign","up"],
-	"cbc":["ET","PM","comments","read"]
+	"bbt":["comments","sign","up","SIGN"],
+	"cbc":["ET","PM","comments","read","AM","video"]
 	};	
 	var rule = function(freq){return (forbid[paper].indexOf(freq[0])===-1);}
 	
@@ -35,16 +35,60 @@ var data = {};
     };
     oReq.open("get", "get-data.php", false);
     oReq.send();
+    /*
+  <!-- Project One -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project One</h3>
+                <h4>Subheading</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+                <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
 
+        <hr> */
+	var full = {
+	"jbn":"Jacobin",
+	"nyt":"New York Times",
+	"dmk":"Daily Maverick",
+	"gdn":"Gaurdian",
+	"bbt":"Breitbart",
+	"cbc":"CBC News"
+	};	
 for (let value of news) {
 
     var content = data[value];
     content = content.replace(/(\\r\\n|\\n|\\r|\\u|\\t|nbsp|raquo|2019s|2019t|201d|00bb|ndash|rsquo)/gm, ' ');
+    var row = document.createElement('div');
+    row.className = "row";
+    
+    var leftcol = document.createElement('div');
+    leftcol.className="col-md-7";
+    row.appendChild(leftcol);
+	    	
     var newCanvas = document.createElement('canvas');
     newCanvas.id = value;
-    newCanvas.width = 400;
-    newCanvas.height = 250;
-    document.getElementById("myDiv").appendChild(newCanvas);
+    newCanvas.width = 700;
+    newCanvas.height = 300;
+    leftcol.appendChild(newCanvas);
+
+    var rightcol = document.createElement('div');
+    rightcol.className="col-md-5";
+    row.appendChild(rightcol);
+
+    var heading = document.createElement('h3');
+    heading.innerText = full[value];
+    rightcol.appendChild(heading);
+
+    var endline = document.createElement('hr');    
+    document.getElementById("myDiv").appendChild(row);
+    document.getElementById("myDiv").appendChild(endline);
 // Create an options object for initialization
 var options = {
   workerUrl: 'scripts/wordfreq.worker.js' };
